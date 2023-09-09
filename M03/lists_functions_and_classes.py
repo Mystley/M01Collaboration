3cce1fa393f11d33ed5128e4af8c4f64b7e594ff
Padley <pperard@ivytech.edu>
2023-09-09 01:18:46 -0400

    Completed M03 lab -pp

---
+++ M03/lists_functions_and_classes.py
@@ -0,0 +1,53 @@
+"""
+This is a programm that takes user information about a vehicle type and display them back to the user using class
+
+Padley Perard
+"""
+
+# Class definitions
+class Vehicle:
+    valid_types = ["car", "truck", "plane", "boat", "broomstick"]
+    vehicle_type = input("Vehicle type: ")
+
+    while vehicle_type not in valid_types:                 # Making sure the user type the right type of vehicle
+        vehicle_type = input("Please enter a valid Vehicle type (car, truck, plane, boat, broomstick): ")
+
+class Automobile(Vehicle):
+    year = int(input("Vehicle year: "))
+    make = input("Vehicle make: ")
+    model = input("Vehicle model: ")
+
+    doors = int(input("Vehicle doors (2 or 4): "))
+    while doors != 2:                                      # Making sure the user type the right numbers
+        if doors == 4:
+            break
+        else:
+            doors = input("Only '2' or '4' is accepted")
+    
+    roof = input("Vehicle roof ('solid' or 'sun roof'): ")
+    while roof != "solid":                                 # Making sure the user type the right type of roof
+        if roof == "sun roof":
+            break
+        else:
+            print("Only 'solid' or 'sun roof' is accepted in all lowercase")
+            roof = input("Vehicle roof ('solid' or 'sun roof'): ")
+    
+
+    
+
+# Instances
+vehicle_type_entry = Automobile.vehicle_type
+year_entry = Automobile.year
+make_entry = Automobile.make
+model_entry = Automobile.model
+doors_entry = Automobile.doors
+roof_entry = Automobile.roof
+
+# Output
+print("\n")
+print(f'Vehicle type:{vehicle_type_entry.rjust(12)}')
+print(f'Year:{year_entry:>21}')
+print(f'Make:{make_entry.rjust(21)}')
+print(f'Model:{model_entry.rjust(20)}')
+print(f'Doors:{doors_entry:>20}')
+print(f'Roof:{roof_entry.rjust(21)}')
